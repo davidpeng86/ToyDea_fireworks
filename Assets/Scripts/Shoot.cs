6f6c0ddf6c19eddc9ddce49cc5a58b0694eb47fa
Dubito-mac <peng86david@gmail.com>
2019-08-30 13:53:34 +0800

    able to shoot

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject obj = Instantiate(prefab, transform, true);
+            Destroy(obj,3);
+        }
+    }
+}
