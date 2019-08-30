d4337f16b2c585f10261e2d92c1fc4db9736c920
Dubito-mac <peng86david@gmail.com>
2019-08-30 14:57:03 +0800

    able to blast

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     public GameObject prefab;
+
+    public float force = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +18,11 @@ public class Shoot : MonoBehaviour
     {
         if(Input.GetMouseButtonDown(0))
         {
-            GameObject obj = Instantiate(prefab, transform, true);
-            Destroy(obj,3);
+            GameObject obj = Instantiate(prefab, transform);
+            obj.transform.parent = null;
+            Rigidbody2D body = obj.GetComponent<Rigidbody2D>();
+            body.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+            //Destroy(obj,3);
         }
     }
 }
---
+++ Assets/Scripts/firework.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class firework : MonoBehaviour
+{
+    Rigidbody2D body;
+    public GameObject blast;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(body.velocity.y <= 0f){
+            blast.SetActive(true);
+        }
+    }
+}
