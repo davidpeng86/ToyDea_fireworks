c96e2077792b70d8f0f7d4f62a6aba6418cbed75
Dubito-mac <peng86david@gmail.com>
2019-08-31 20:43:28 +0800

    ready to build

---
+++ Assets/Scripts/ScreenRes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenRes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(828, 1792, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
