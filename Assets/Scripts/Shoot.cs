668732d2dcc40efe20747e3db4b9a8d316cd0629
25077667 <zxc25077667@gmail.com>
2019-08-31 19:10:50 +0800

    fix warning and canvas show at finished

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -7,11 +7,11 @@ public class Shoot : MonoBehaviour
     public int coolDown = 4;
     //public ScoreTracker scoreTracker;
     [SerializeField]
-    GameObject prefab;
+    public GameObject prefab;
     public static int shootNum = 0;
     [SerializeField]
     float force = 10;
-    Transform camera;
+    Transform transformedCamera;
     Sequence s ;
 
     float timer = 0;
@@ -19,21 +19,22 @@ public class Shoot : MonoBehaviour
     void Start()
     {
         s = DOTween.Sequence( );
-        camera = Camera.main.transform;
+        transformedCamera = Camera.main.transform;
         timer = 4f;
+        coolDown = 4;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(0) && timer>coolDown)
+        if(Input.GetMouseButtonDown(0) && timer > coolDown)
         {
             timer = 0;
             GameObject obj = Instantiate(prefab, transform);
             obj.transform.parent = null;
             DOTween.CompleteAll();
-            s.Append(camera.DOShakePosition(0.15f,new Vector2(0.2f,0.6f),6,0)).OnComplete(() => 
-            camera.position = Vector2.Lerp(camera.position,Vector2.zero,0.5f));
+            s.Append(transformedCamera.DOShakePosition(0.15f,new Vector2(0.2f,0.6f),6,0)).OnComplete(() => 
+            transformedCamera.position = Vector2.Lerp(transformedCamera.position,Vector2.zero,0.5f));
             Rigidbody2D body = obj.GetComponent<Rigidbody2D>();
             body.AddForce(Vector2.up * force, ForceMode2D.Impulse);
         }
