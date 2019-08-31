0944597d8137ac02ae3a7826f9cde66b05ebf83d
25077667 <zxc25077667@gmail.com>
2019-08-31 13:54:31 +0800

    better coding style

--- Assets/Scripts/firework.cs
+++ Assets/Scripts/firework.cs
@@ -5,8 +5,8 @@ using DG.Tweening;
 public class firework : MonoBehaviour
 {
     Rigidbody2D body;
-    Transform camera;
-    bool explode = true;
+    Transform transformedCamera;
+    bool canExplode = true;
     [SerializeField]
     GameObject blast;
     SpriteRenderer renderer;
@@ -14,7 +14,7 @@ public class firework : MonoBehaviour
     void Start()
     {
         ZoomTrigger.zoomReady = true;
-        camera = Camera.main.transform;
+        transformedCamera = Camera.main.transform;
         body = GetComponent<Rigidbody2D>();
         renderer = GetComponent<SpriteRenderer>();
         Shoot.shootNum ++;
@@ -23,8 +23,8 @@ public class firework : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(body.velocity.y <= 0f && explode == true){
-            camera.DOShakePosition(0.2f,new Vector2(0.4f,0.6f),20,60);
+        if(body.velocity.y <= 0f && canExplode){
+            transformedCamera.DOShakePosition(0.2f, new Vector2(0.4f, 0.6f), 20, 60);
             blast.SetActive(true);
             blast.transform.parent = null;
             if (Shoot.shootNum > 3 || Shoot.shootNum < 0)
@@ -37,7 +37,7 @@ public class firework : MonoBehaviour
     }
     private void OnCollisionEnter2D(Collision2D other) {
         if(other.gameObject.tag == "obstacles"){
-            explode = false;
+            canExplode = false;
             Destroy(gameObject,3);
         }
     }
