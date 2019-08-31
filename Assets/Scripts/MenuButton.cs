6d73b37cacb6ad62323e51c63c9e8f383b7eed09
25077667 <zxc25077667@gmail.com>
2019-08-31 14:23:04 +0800

    reduce for loop

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -8,11 +8,11 @@ public class MenuButton : MonoBehaviour
 
     Vector3 mousePreviousPOS = Vector3.zero;
     bool track;
-    int selected = 0;
+    int onSelected = 0;
     // Start is called before the first frame update
     void Start()
     {
-        selected = 0;
+        onSelected = 0;
         track = false;
     }
 
@@ -20,47 +20,46 @@ public class MenuButton : MonoBehaviour
     void Update()
     {
 
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             track = true;
         }
-        if(Input.GetMouseButtonUp(0))
+        if (Input.GetMouseButtonUp(0))
         {
             track = false;
         }
-        if(track == true){
+        if (track == true)
+        {
             TrackMouse();
-        }   
-        
+        }
+
     }
 
-    void TrackMouse(){
+    void TrackMouse()
+    {
         float difference = Input.mousePosition.x - mousePreviousPOS.x;
-            if(Mathf.Abs(difference)> 10)
+        if (Mathf.Abs(difference) > 10)
+        {
+            if (onSelected >= 0 && onSelected <= 5)
             {
-                if(selected >= 0 && selected <= 5){
-                    if(difference > 0 && selected < 5)
-                            selected++;
-                    if(difference < 0 && selected > 0)
-                            selected--;
-                }
-                for(int i = 0; i < buttons.Length; i++){
-                    if(i == selected){
-                        buttons[i].isSelected = true;
-                        transform.rotation = Quaternion.Euler(0,0,buttons[i].angle);
-                    }
-                    else{
-                        buttons[i].isSelected = false;
-                    }
-                }
-                // if(Mathf.Cos(Mathf.Deg2Rad*transform.eulerAngles.z) > Mathf.Cos(Mathf.Deg2Rad*16) ) 
-                // {
-                //     transform.Rotate(0f,0f,-5f*Mathf.Sign(difference),Space.World);
-                // }
+                if (difference > 0 && onSelected < 5)
+                    onSelected++;
+                if (difference < 0 && onSelected > 0)
+                    onSelected--;
             }
-            mousePreviousPOS = Input.mousePosition;
-            
+            for (int i = 0; i < buttons.Length; i++)
+                buttons[i].isSelected = false;  //return all seleted to zero
+
+            buttons[onSelected] = true;
+            transform.rotation = Quaternion.Euler(0, 0, buttons[onSelected].angle);
+            // if(Mathf.Cos(Mathf.Deg2Rad*transform.eulerAngles.z) > Mathf.Cos(Mathf.Deg2Rad*16) ) 
+            // {
+            //     transform.Rotate(0f,0f,-5f*Mathf.Sign(difference),Space.World);
+            // }
+        }
+        mousePreviousPOS = Input.mousePosition;
+
     }
 
-    
+
 }
