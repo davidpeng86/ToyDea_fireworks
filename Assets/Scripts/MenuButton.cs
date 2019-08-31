89e2a8429bf668757fb61f7ce40e2641a7fb3bc5
25077667 <zxc25077667@gmail.com>
2019-08-31 11:13:07 +0800

    Zoom in smooth

--- Assets/Scripts/MenuButton.cs
+++ Assets/Scripts/MenuButton.cs
@@ -19,7 +19,6 @@ public class MenuButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         if(Input.GetMouseButtonDown(0))
         {
             track = true;
--- Assets/Scripts/ZoomTrigger.cs
+++ Assets/Scripts/ZoomTrigger.cs
@@ -6,13 +6,15 @@ public class ZoomTrigger : MonoBehaviour
 {
     // Start is called before the first frame update
     public static bool zoomReady = true;
-    Camera camera;
+    Camera mainCamera;
     bool enter;
     bool exit;
+    const int zoomSpeed = 20;
+
     void Start()
     {
         zoomReady = true;
-        camera = Camera.main;
+        mainCamera = Camera.main;
         enter = false;
         exit = false;
     }
@@ -20,27 +22,36 @@ public class ZoomTrigger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(enter){
+        if (enter)
+        {
             //計時realtime並slerp至transform
             //zoom in
+            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, transform.position, 0.5f);
+            mainCamera.orthographicSize = Mathf.SmoothStep(5, 1, 0.5f);
             enter = false;
         }
 
-        if(exit){
+        if (exit)
+        {
             // slerp至(0,0)
             //zoom out
+            mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, Vector2.zero, 0.5f);
+            mainCamera.orthographicSize = Mathf.SmoothStep(1, 5, 0.5f);
             exit = false;
         }
     }
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(zoomReady){
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (zoomReady)
+        {
             zoomReady = false;
             enter = true;
             Time.timeScale = 0.2f;
             Time.fixedDeltaTime *= 0.2f;
         }
     }
-    private void OnTriggerExit2D(Collider2D other) {
+    private void OnTriggerExit2D(Collider2D other)
+    {
         exit = true;
         Time.timeScale = 1f;
         Time.fixedDeltaTime *= 5f;
