9e02bf44a76f2949c1a29f27af9c3a7ccfc6977a
Dubito-mac <peng86david@gmail.com>
2019-09-01 11:09:52 +0800

    added bunch of stuff

--- Assets/Scripts/ScreenRes.cs
+++ Assets/Scripts/ScreenRes.cs
@@ -7,7 +7,7 @@ public class ScreenRes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(828, 1792, false);
+        Screen.SetResolution(828*2, 1792*2, false);
     }
 
     // Update is called once per frame
