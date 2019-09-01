73eea2b2428598a25e4d41f5a614aa4f5c5c83df
Dubito-mac <peng86david@gmail.com>
2019-09-01 11:51:58 +0800

    added button script

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Buttons : MonoBehaviour
+{
+    Scene scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void NextStage(){
+        SceneManager.LoadScene(scene.buildIndex+1, LoadSceneMode.Single);
+    }
+    public void Replay(){
+        SceneManager.LoadScene(scene.buildIndex, LoadSceneMode.Single);
+    }
+    public void Home(){
+        SceneManager.LoadScene("StartScene", LoadSceneMode.Single);
+    }
+}
